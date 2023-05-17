a9c52a3a7448a9b507842a633584bd8f2fded080
selvisentest2006 <selvisentest2006@gmail.com>
2023-05-17 11:05:23 +0530

    added lazyload-search-pagination

---
+++ src/components/Home.js
@@ -0,0 +1,29 @@
+
+import Products from "./Products";
+
+const Home = () => {
+    return (
+      <div className="hero">
+        <div className="card text-bg-dark border-0">
+          <img
+            src="/assets/bg.jpg"
+            className="card-img"
+            alt="Background"
+            height="550px"
+          />
+          <div className="card-img-overlay d-flex flex-column justify-content-center">
+            <div className="container">
+              <h5 className="card-title display-3 fw-bolder mb-0">New Product</h5>
+              <p className="card-text ">
+                CHECK OUT ALL THE PRODUCTS
+              </p>
+             
+            </div>
+          </div>
+        </div>
+       <Products />
+      </div>
+    );
+  };
+  
+  export default Home;
